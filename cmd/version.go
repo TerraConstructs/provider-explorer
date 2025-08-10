4c2c28e437b8fdc3a30b881ddec4c7b46cbd0313
so0k <vincent.drl@gmail.com>
2025-08-10 09:11:24 +0700

    chore: Add release-please

--- cmd/version.go
+++ cmd/version.go
@@ -6,11 +6,28 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Build-time variables set by GoReleaser
+var (
+	version = "dev"
+	commit  = "unknown"
+	date    = "unknown"
+	builtBy = "unknown"
+)
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Provider Explorer v0.1.0")
+		fmt.Printf("Provider Explorer %s\n", version)
+		if commit != "unknown" {
+			fmt.Printf("Commit: %s\n", commit)
+		}
+		if date != "unknown" {
+			fmt.Printf("Built: %s\n", date)
+		}
+		if builtBy != "unknown" {
+			fmt.Printf("Built by: %s\n", builtBy)
+		}
 	},
 }
 
